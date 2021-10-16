598aeb565a661126b380cecc820323d7924504d5
Natanael Vitorino <64169072+natanael19vitorino@users.noreply.github.com>
2021-10-16 19:16:11 -0300

    Lambs automationd created

--- firebase-esp.py
+++ firebase-esp.py
@@ -18,14 +18,50 @@ if(sta_if.isconnected() == False):
 print("Connected")
 print(sta_if.ifconfig())
 
-led = Pin(22, Pin.OUT)
+ledKitchen        = Pin(22, Pin.OUT)
+ledCoupleRoom     = Pin(23, Pin.OUT)
+ledLivingRoom     = Pin(25, Pin.OUT)
+ledBathroom       = Pin(26, Pin.OUT)
+ledGarage         = Pin(27, Pin.OUT)
+
+def houseLamps():
+
+    kitchen         = firebase.get("https://proud-limiter-323718-default-rtdb.firebaseio.com/kitchen/lamp")
+    coupleRoom      = firebase.get("https://proud-limiter-323718-default-rtdb.firebaseio.com/coupleRoom/lamp")
+    livingRoom      = firebase.get("https://proud-limiter-323718-default-rtdb.firebaseio.com/livingRoom/lamp")
+    bathroom        = firebase.get("https://proud-limiter-323718-default-rtdb.firebaseio.com/bathRoom/lamp")
+    garege          = firebase.get("https://proud-limiter-323718-default-rtdb.firebaseio.com/garage/lamp")
+
+    # Kitchen
+    if kitchen == 1:
+        ledKitchen.value(1)
+    elif kitchen == 0:
+        ledKitchen.value(0)
+
+    # Couple Room
+    if coupleRoom == 1:
+        ledCoupleRoom.value(1)
+    elif coupleRoom == 0:
+        ledCoupleRoom.value(0)
+    
+    # Living Room
+    if livingRoom == 1:
+        ledLivingRoom.value(1)
+    elif livingRoom == 0:
+        ledLivingRoom.value(0)
+
+    # Bathroom
+    if bathroom == 1:
+        ledBathroom.value(1)
+    elif bathroom == 0:
+        ledBathroom.value(0)
+
+    # Garage
+    if garege == 1:
+        ledGarage.value(1)
+    elif garege == 0:
+        ledGarage.value(0)
+
 
 while True:
-    ledApi = firebase.get("https://iot-esp32-8b0e5-default-rtdb.firebaseio.com/IoT/led")
-    print(ledApi)
-    if ledApi == '1':
-        print("LED ON")
-        led.value(1)
-    elif ledApi == '0':
-        print("LED OFF")
-        led.value(0)
+    houseLamps()
